5c9bcb43dc1bee44e3cc199bccc773b1572ad02a
Mark Koyanagi <blindzombie@gmail.com>
2014-07-23 17:13:31 -0400

    Added getContainer call

--- js/basic.js
+++ js/basic.js
@@ -1,4 +1,4 @@
-var map = L.map('map').setView([35.994033, -78.898619], 13);
+var map = L.map('map').setView([35.994033, -78.898619], 14);
 
 L.tileLayer('https://{s}.tiles.mapbox.com/v3/petrock.j1ce2b33/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
@@ -10,3 +10,7 @@ var circle = L.circle([35.994033, -78.898619], 500, {
     fillColor: '#f03',
     fillOpacity: 0.5
 }).addTo(map);
+
+var container = map.getContainer();
+
+console.log(container);
