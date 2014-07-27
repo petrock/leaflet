eeea5020c8b553a1ed8db20d9ed16af19926bb2a
Mark Koyanagi <blindzombie@gmail.com>
2014-07-27 19:16:13 -0400

    Switched tile source to OSM

--- js/layer.js
+++ js/layer.js
@@ -1,13 +1,15 @@
 var mapCenter = [35.994033, -78.898619];
-var map = L.map('map').setView(mapCenter, 14);
+var map = L.map('map').setView(mapCenter, 11);
+var mapLink = '<a href="http://openstreetmap.org">OpenStreetMap</a>';
+
 var merge = L.marker([35.997057, -78.899455]).bindPopup('Merge Records')
 var places = L.layerGroup([merge]);
 
 var imageUrl = 'images/security_savings.jpg',
     imageBounds = [[36.020781,-78.935652],[35.990924,-78.887136]];
 
-L.tileLayer('https://{s}.tiles.mapbox.com/v3/petrock.j1ce2b33/{z}/{x}/{y}.png', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data &copy; ' + mapLink,
     maxZoom: 18
 }).addTo(map);
 
@@ -24,5 +26,5 @@ var overlayMaps = {
 L.control.layers(overlayMaps).addTo(map);
 
 L.imageOverlay(imageUrl, imageBounds, {
-    opacity: 0.3
+    opacity: 0.7
 }).addTo(map);
