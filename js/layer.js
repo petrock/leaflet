32226790c816db5e5518dc70fa1aacc55670c7a4
Mark Koyanagi <blindzombie@gmail.com>
2014-07-30 07:43:01 -0400

    Added base layers

--- js/layer.js
+++ js/layer.js
@@ -1,7 +1,14 @@
 var mapCenter = [35.994033, -78.898619];
-var map = L.map('map').setView(mapCenter, 13);
-var mapLink = '<a href="http://openstreetmap.org">OpenStreetMap</a>';
 
+var osmLink = '<a href="http://openstreetmap.org">OpenStreetMap</a>',
+    thunLink = '<a href="http://thunderforest.com/">Thunderforest</a>';
+
+var osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', 
+    osmAttrib = '&copy; ' + osmLink + ' Contributors',
+    landUrl = 'http://{s}.tile.thunderforest.com/landscape/{z}/{x}/{y}.png',
+    thunAttrib = '&copy; '+osmLink+' Contributors & '+thunLink;
+
+// marker
 var merge = L.marker([35.997057, -78.899455],
         {draggable: true,
         title: 'Merge Records',
@@ -12,34 +19,52 @@ var places = L.layerGroup([merge]);
 var imageUrl = 'images/security_savings.jpg',
     imageBounds = [[35.950781,-78.975652],[35.990924,-78.927136]];
 
-L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: 'Map data &copy; ' + mapLink,
-    maxZoom: 18
-}).addTo(map);
+// add map tiles layers
+var osmMap = L.tileLayer(osmUrl, {attribution: osmAttrib}),
+    landMap = L.tileLayer(landUrl, {attribution: thunAttrib});
 
+var map = L.map('map', {
+    layers: [osmMap]
+})
+.setView(mapCenter, 13);
+
+
+// circle
 var circle = L.circle(mapCenter, 500, {
     color: 'red',
     fillColor: '#f03',
     fillOpacity: 0.5
 }).addTo(map);
 
+// add base layers
+var baseMaps = {
+    "OSM Map": osmMap,
+    "Landscape": landMap
+};
+
+L.control.layers(baseMaps).addTo(map);
+
+// add marker layer
 var overlayMaps = {
     "Places": places
 };
 
 L.control.layers(overlayMaps).addTo(map);
 
+// image overlay
 L.imageOverlay(imageUrl, imageBounds, {
     opacity: 0.7
 }).addTo(map);
 
+// polyline
 var polyline = L.polyline([
       [35.986132, -78.916297],
       [36.000368, -78.904967],
       [35.997869, -78.879476]
       ],
       {
-          color: '#FFB45D',
+        color: '#FFB45D',
         weight: 8,
-        opacity: 0.7
+        opacity: 0.9,
+        dashArray: '25, 20'
       }).addTo(map);
